0652bb58ec531117dbd81f91151e84221531f7af
KasperVaessen <58064813+KasperVaessen@users.noreply.github.com>
2020-01-15 16:18:53 +0100

    timer js

---
+++ public/javascripts/timer.js
@@ -0,0 +1,27 @@
+let minutes = 0;
+let seconds = 0;
+setInterval(function() {
+    seconds++;
+    if(seconds == 60) {
+        minutes++;
+        seconds = 0;
+    }
+    let secondStr
+    let minuteStr
+    if(seconds < 10) {
+        secondStr = '0' + seconds
+    }
+    else {
+        secondStr = seconds
+    }
+
+    if(minutes < 10) {
+        minuteStr = '0' + minutes;
+    }
+    else {
+        minuteStr = minutes;
+    }
+    let time = (minuteStr + ':' + secondStr)
+    let el = document.getElementById("timer")
+    el.innerHTML = time;
+}, 1000)
